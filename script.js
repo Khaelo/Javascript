c309a83a9568eaeef827efbc86d8176c93ee0913
Luca Bruno <luca.bruno@ynov.com>
2019-01-28 14:42:38 +0100

    First

---
+++ script.js
@@ -0,0 +1,15 @@
+//'use strict';
+
+//console.log('Bonjour');
+
+
+let username;
+username = 'Luca';
+username = 'Toto';
+username = 'Tata';
+username = 'Titi';
+username = 3;
+
+const userStatus = 1;
+
+// let username = 'Luca';
