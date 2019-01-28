80f4b166f0b7f7ccb97efab7d645a77614548779
Luca Bruno <luca.bruno@ynov.com>
2019-01-28 15:15:52 +0100

    Operator

--- script.js
+++ script.js
@@ -10,6 +10,63 @@ username = 'Tata';
 username = 'Titi';
 username = 3;
 
+// let username = 'Luca';
+
+let myString1 = "Hello World";
+let myString2 = 'Hello World';
+let myString3 = `
+Hello
+`;
+
 const userStatus = 1;
 
-// let username = 'Luca';
+let myNumber = 1.5;
+
+let myBoolean = true;
+
+let myUndefined = undefined;
+
+let myNull = null;
+
+//NaN
+
+console.log('Hello' + ' World'); 
+
+const str1 = 'Hello';
+const str2 = 'World';
+console.log(`${str1} ${str2}`);
+
+let a = 10 + 32;
+a +=8;
+console.log('a', a);
+
+let b = 10 - 5;
+b -=8;
+console.log('b', b);
+
+let c = 5 * 7;
+c *=2;
+console.log('c', c);
+
+let d = 10 / 2;
+d /=2;
+console.log('d', d);
+
+let e = 5;
+console.log(e++);
+console.log(e);
+
+let f = 5;
+console.log(f--);
+console.log(f);
+
+console.log([1, 2, 3] * 30);
+
+console.log(+'3');
+console.log(+true);
+
+console.log('4' + 3); //'43'
+console.log(3 + 4 + '7'); //'77'
+console.log('3' + 4 + 7); //'347'
+
+//Concatenation
